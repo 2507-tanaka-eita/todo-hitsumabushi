b1ddea52af0bd535bb14a20575088f00313f3cda
tanaka_eita <tanaka.eita@alhinc.jp>
2025-08-21 11:52:22 +0900

    first-commit

---
+++ src/main/java/com/example/todo_hitsumabushi/TodoHitsumabushiApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_hitsumabushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHitsumabushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoHitsumabushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_hitsumabushi/TodoHitsumabushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_hitsumabushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoHitsumabushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
